8603e5a018894cccccb2f28c4366a1b6e9192915
Steven Xiang <steven.xiang22@imperial.ac.uk>
2024-10-15 18:35:42 +0100

    Implement loading data

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,12 @@
 import numpy as np
 
+dataset = np.loadtxt("../../wifi_db/clean_dataset.txt")
+print(dataset.shape)
 
+x = dataset[:, :-1]
+print(x)
+print(x.shape)
+
+y = dataset[:, -1]
+print(y)
+print(y.shape)
