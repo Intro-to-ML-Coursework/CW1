b3b40e099c79b47f1cf9dae182948e4412c651bb
Steven Xiang <steven.xiang22@imperial.ac.uk>
2024-10-22 20:34:37 +0100

    Refine visualise.py

--- src/visualisation/visualise.py
+++ src/visualisation/visualise.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.models.train_model import decision_tree_learning
 
-def plot_decision_tree(tree, depth, current_depth=0, x_min=0, x_max=1, y=0):
+
+def plot_decision_tree(tree, x_min=0., x_max=1., y=0.):
     """
     Recursively plots the decision tree using matplotlib.
 
     Parameters:
     tree (dict): The decision tree to visualize.
-    depth (int): The depth of the decision tree.
-    current_depth (int): The current depth of the node (used for recursion).
     x_min (float): The left boundary of the node's x-position.
     x_max (float): The right boundary of the node's x-position.
     y (float): The y-position of the node (corresponds to depth).
@@ -32,34 +31,35 @@ def plot_decision_tree(tree, depth, current_depth=0, x_min=0, x_max=1, y=0):
 
     # Plot the children recursively
     if tree['left'] is not None:
-        # Draw a line to the left child
+        # Draw a line between parent and left child
         child_x = (x_min + x) / 2
-        plt.plot([x, child_x], [-y, -(y + 1)], 'k-')  # Draw a line between parent and child
-        plot_decision_tree(tree['left'], depth, current_depth + 1, x_min, x, y + 1)
+        plt.plot([x, child_x], [-y, -(y + 6)], 'k-')
+
+        plot_decision_tree(tree['left'], x_min, x, y + 6)
 
     if tree['right'] is not None:
-        # Draw a line to the right child
+        # Draw a line between parent and right child
         child_x = (x_max + x) / 2
-        plt.plot([x, child_x], [-y, -(y + 1)], 'k-')  # Draw a line between parent and child
-        plot_decision_tree(tree['right'], depth, current_depth + 1, x, x_max, y + 1)
+        plt.plot([x, child_x], [-y, -(y + 6)], 'k-')
+
+        plot_decision_tree(tree['right'], x, x_max, y + 6)
+
+
+def plot_and_save_to(tree, depth, path):
+    # Set up the plot
+    plt.figure(figsize=(60, 6 * depth))
+    plt.axis('off')
+
+    # Plot the decision tree
+    plot_decision_tree(tree, depth)
+
+    # Save the plot
+    plt.savefig(path)
 
 
 # Example usage
 if __name__ == '__main__':
     dataset_clean = np.loadtxt("../../wifi_db/clean_dataset.txt")
-    tree, tree_depth = decision_tree_learning(dataset_clean, 0)
+    decisionTree, tree_depth = decision_tree_learning(dataset_clean, 0)
 
-    # Set up the plot
-    plt.figure(figsize=(60, 6 * tree_depth))  # Dynamically adjust the figure size based on depth
-    plt.axis('off')  # Turn off the axis
-
-    # Plot the decision tree
-    plot_decision_tree(tree, tree_depth)
-
-    # Save the plot
-    plt.savefig("tree.png")
-
-    # Display the plot
-    plt.show()
-
-
+    plot_and_save_to(decisionTree, tree_depth, "tree.png")
